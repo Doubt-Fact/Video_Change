89ca662fa0716b47c61a402ef1bcbd4a0a1b3d65
Doubt-Fact <jiyiming@doubt-fact.top>
2022-08-20 22:24:53 +0800

    源码-新增更改保存位置功能

--- 视频格式转换.py
+++ 视频格式转换.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter.filedialog import askdirectory,askopenfilename
 # from sys import getdlopenflags
 from fileinput import filename
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
@@ -6,7 +9,10 @@ from PyQt5.QtCore import *
 import ffmpy
 import calendar
 import time
+import os
 
+
+set_file = 2
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
 
@@ -28,15 +34,15 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "音视频转换工具箱-v1.1"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "音视频转换工具箱-v1.2"))
         self.pushButton = QtWidgets.QPushButton(self.centralWidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 30, 200, 30))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton.setText("选择要转换的视频")
+        self.pushButton.setText("选择文件并开始格式转换")
         self.pushButton.clicked.connect(self.openfile)
 
         self.labelA = QtWidgets.QLabel(MainWindow)
-        self.labelA.setText('此工具可一键转换视频格式（复制\n音视频，在保持音视频质量基本不\n变的情况下改变音视频格式。注意，\n音视频分离功能仅支持音频aac编码\n的素材）')
+        self.labelA.setText('此工具可一键转换视频格式（复制\n音视频，在保持音视频质量保持不\n变的情况下改变音视频格式。注意，\n音视频分离功能仅支持音频aac编码\n的素材。）')
         self.labelA.setGeometry(QtCore.QRect(240, 30, 250, 80))
 
         self.labelB = QtWidgets.QLabel(MainWindow)
@@ -47,7 +53,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pushButton2 = QtWidgets.QPushButton(self.centralWidget)
         self.pushButton2.setGeometry(QtCore.QRect(10, 80, 200, 30))
         self.pushButton2.setObjectName("pushButton")
-        self.pushButton2.setText("选择要音视频分离的文件")
+        self.pushButton2.setText("选择文件并开始音视频分离")
         self.pushButton2.clicked.connect(self.openfile2)
 
         self.labelC = QtWidgets.QLabel(MainWindow)
@@ -60,6 +66,39 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.labelD.setOpenExternalLinks(True)
         self.labelD.setGeometry(QtCore.QRect(100, 180, 380, 80))
 
+        self.pushButton_file = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_file.setGeometry(QtCore.QRect(10, 130, 200, 30))
+        self.pushButton_file.setObjectName("pushButton")
+        self.pushButton_file.setText("选择文件保存地址")
+        self.pushButton_file.clicked.connect(self.openfile3)
+        # self.labelE.setText(directory)
+
+        self.labelE = QtWidgets.QLabel(MainWindow)
+        self.labelE.setText('在转换前如需更改文件地址，请单击上方按钮')
+        self.labelE.setOpenExternalLinks(True)
+        self.labelE.setGeometry(QtCore.QRect(50, 170, 380, 20))
+
+
+
+
+
+    def openfile3(self):
+        ts1 = time.strftime('%H%M%S')
+        global directory
+        global set_file
+        # fileName_choose, filetype = QFileDialog.getOpenFileName(self,  
+        #                             "选取文件",  
+        #                             '', # 起始路径 
+        #                             "All Files (*.*)")
+        directory = QtWidgets.QFileDialog.getExistingDirectory(None,"选取文件夹","")
+        # print(directory)
+        directory = directory+'/'
+        self.labelE.setText(directory)
+        set_file = 1
+
+
+
+
     def openfile(self):
         global dizhi
         ts1 = time.strftime('%H%M%S')
@@ -68,12 +107,28 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
                                     '', # 起始路径 
                                     "All Files (*.*)")
         dizhi = fileName_choose
-        ff = ffmpy.FFmpeg(
-            inputs={dizhi: None},
-            # outputs={mingming+'.mp4': None}
-            outputs={dizhi+ts1+'.mp4': '-c:v copy -c:a copy'}
-            )
-        ff.run() 
+
+        
+
+        if set_file == 1:
+
+            dirt_0, suffix = os.path.splitext(fileName_choose)	# 获取后缀之前的部分和后缀，返回元组数据类型
+            dirt_1, file_name = os.path.split(dirt_0)	# 获取文件目录，文件，返回元组数据类型
+            # print(file_name)
+
+            ff = ffmpy.FFmpeg(
+                inputs={dizhi: None},
+                # outputs={mingming+'.mp4': None}
+                outputs={directory+file_name+ts1+'.mp4': '-c:v copy -c:a copy'}
+                )
+            ff.run() 
+        else:
+            ff = ffmpy.FFmpeg(
+                inputs={dizhi: None},
+                # outputs={mingming+'.mp4': None}
+                outputs={dizhi+ts1+'.mp4': '-c:v copy -c:a copy'}
+                )
+            ff.run() 
 
     def openfile2(self):
         ts1 = time.strftime('%H%M%S')
@@ -83,12 +138,27 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
                                     '', # 起始路径 
                                     "All Files (*.*)")
         dizhi2 = fileName_choose
-        ff = ffmpy.FFmpeg(
-            inputs={dizhi2: None},
-            outputs={dizhi2+ts1+'.aac': '-c:a copy -vn',dizhi2+ts1+'.mp4':'-c:v copy -an'}
-            )
-        ff.run() 
-        
+
+        if set_file == 1:
+
+            dirt_0, suffix = os.path.splitext(fileName_choose)	# 获取后缀之前的部分和后缀，返回元组数据类型
+            dirt_1, file_name = os.path.split(dirt_0)	# 获取文件目录，文件，返回元组数据类型
+            # print(file_name)
+
+            ff = ffmpy.FFmpeg(
+                inputs={dizhi2: None},
+                outputs={directory+file_name+ts1+'.aac': '-c:a copy -vn',directory+file_name+ts1+'.mp4':'-c:v copy -an'}
+                )
+            ff.run()
+
+        else:
+
+            ff = ffmpy.FFmpeg(
+                inputs={dizhi2: None},
+                outputs={dizhi2+ts1+'.aac': '-c:a copy -vn',dizhi2+ts1+'.mp4':'-c:v copy -an'}
+                )
+            ff.run() 
+
 
 
 
